0e4f2099f604fe7bbfedb93b57a2b9da0b871ac4
djcross930 <dancross96@gmail.com>
2023-04-11 11:26:24 -0500

    Made the api url dynamic to the current date using the LocalDate java class

--- src/main/java/com/example/springindex/ApiController.java
+++ src/main/java/com/example/springindex/ApiController.java
@@ -4,16 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
+import java.time.LocalDate;
+
 
 @Controller
 public class ApiController {
 
     @GetMapping("/")
     public String index(Model model) {
+        // Get the current date
+        LocalDate currentDate = LocalDate.now();
+        int currentDay = currentDate.getDayOfMonth();
+        int currentMonth = currentDate.getMonthValue();
+
+        // Replace the hardcoded day and month with the current day and month
+        String apiUrl = "https://byabbe.se/on-this-day/" + currentMonth + "/" + currentDay + "/events.json";
+
         RestTemplate restTemplate = new RestTemplate();
-        String apiUrl = "https://byabbe.se/on-this-day/7/7/events.json";
         ApiResponse apiResponse = restTemplate.getForObject(apiUrl, ApiResponse.class);
+
         model.addAttribute("events", apiResponse.getEvents());
+
         return "index";
     }
 
