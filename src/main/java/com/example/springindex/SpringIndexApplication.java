bac4507dddad0365f6852c2286daebe6751a53db
djcross930 <dancross96@gmail.com>
2023-04-10 21:42:22 -0500

    skeleton

---
+++ src/main/java/com/example/springindex/SpringIndexApplication.java
@@ -0,0 +1,13 @@
+package com.example.springindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIndexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIndexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springindex/SpringIndexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springindex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIndexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
